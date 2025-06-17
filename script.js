6a5775ee589ba34b9de21c6b8cbc69b96a90c880
Rithesh-2908 <ritheshgiriprasad@gmail.com>
2025-06-17 13:24:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Welcome to the purple-themed Prodigy website!");
+
+  window.applyNow = function () {
+    alert("Thanks for applying! We’ll contact you soon.");
+  }
+});
